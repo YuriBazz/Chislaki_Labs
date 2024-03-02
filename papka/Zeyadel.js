061378007da2a621aee4385c2edee2de36f2a528
Yuri Kovach <gim.ekb12@gmail.com>
2024-03-02 14:35:34 +0500

    Перенос

--- papka/Zeyadel.js
+++
@@ -1,6 +0,0 @@
-import * as mathjs from 'mathjs';
-//Считал я тоже тут, но в ходе придумывания читаемого кода все удалил, поэтому рассчетов тут нету ;(
-let B = [[0, 0.08,-1.01],[0,0.07,-1.05],[0,-0.07,1.04]];
-export function Zeydel(){
-    console.log(mathjs.eigs(B).values);
-}
